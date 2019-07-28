c84ea27ee4cdd3d44a39b004f479f388191e8d45
Nate Murray <nate@natemurray.com>
2019-07-28 11:49:24 -0500

    feat: add uppercase

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,11 @@ function handleHelloWorld(_: Request, res: Response) {
   res.json({ msg: 'Hello, world' });
 }
 
+function handleUppercase(req: Request, res: Response) {
+  const message = req.query.msg || 'no message given';
+  res.json({ msg: message.toUpperCase() });
+}
+
 function handleReverse(req: Request, res: Response) {
   res.json({
     msg: req.params.msg
@@ -18,7 +23,6 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', handleHelloWorld);
-app.get('/reverse/:msg', handleReverse);
 
 const server = app.listen(app.get('port'), () => {
   console.log('App is running at http://localhost:%d', app.get('port'));
